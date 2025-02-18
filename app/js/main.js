47f1998adcb10bd33f3f9c1a7c339b8e11ae0813
Roma Vintonyak <19roman23@gmail.com>
2025-02-18 19:29:18 +0200

    create footer&intro section

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 jQuery(document).ready(function () {
   "use script";
   /*burger btn*/
-  $(".burger__btn").on("click", function(){
+  $(".burger__btn").on("click", function () {
     $(this).toggleClass("burger__btn--active");
     $(".menu").toggleClass("menu__open");
+  });
+  /*lightbox*/
+  lightbox.option({
+    'resizeDuration': 300,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true,
+    'albumLabel': "Зображення %1 із %2",
+    'disableScrolling': true
   });
 });
