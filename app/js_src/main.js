9537d490cf7b80aae4cbeb3d2e745e323a16e0e4
Roma Vintonyak <19roman23@gmail.com>
2025-02-19 20:00:52 +0200

    create modal form

--- app/js_src/main.js
+++ app/js_src/main.js
@@ -13,4 +13,28 @@ jQuery(document).ready(function () {
     'albumLabel': "Зображення %1 із %2",
     'disableScrolling': true
   });
+  /*fixed header*/
+  var headerHeight = $("#header").height();
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >= headerHeight) {
+      $("#header").addClass("header__fixed");
+    } else {
+      $("#header").removeClass("header__fixed");
+    }
+  });
+  /*backtop btn*/
+  var introH = $(".intro").height();
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >= (introH || 800)) {
+      $(".backtop").addClass("backtop__active");
+    } else {
+      $(".backtop").removeClass("backtop__active");
+    }
+  });
+  $(".backtop").on("click", function () {
+    $("html, body").animate({
+      scrollTop: 0,
+    }, 1000, "swing");
+  });
+
 });
